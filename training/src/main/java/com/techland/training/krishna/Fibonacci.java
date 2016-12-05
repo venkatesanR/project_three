d04cc199f4fb57e86ec7b5a48f89657a4021ee1c
krishna0592 <mohanakrishnan0592@gmail.com>
2016-12-05 23:02:18 +0530

    Fibonacci.java

--- training/src/main/java/com/techland/training/krishna/Fibonacci.java
+++ training/src/main/java/com/techland/training/krishna/Fibonacci.java
@@ -1,6 +1,5 @@
 package com.techland.training.krishna;
 
-import java.awt.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,7 +8,6 @@ public class Fibonacci {
 	static int userInput;
 	static int limit = 5;
 	static ArrayList<Integer> list;
-	static ArrayList<Integer> finalList;
 
 	public static int userinput() {
 		Scanner scan = new Scanner(System.in);
@@ -35,38 +33,34 @@ public class Fibonacci {
 		return list;
 	}
 
-	public static ArrayList<Integer> sequence(ArrayList<Integer> list) {
+	public static void sequence(ArrayList<Integer> list) {
 		int startValue;
 		int nextValue;
+
 		if (list.contains(userInput)) {
 			System.out.println("prime");
 			startValue = userInput;
-//			System.out.println(startValue);
 			nextValue = list.get(list.size() - 2) + startValue;
-//			System.out.println(nextValue);
 		} else {
 			System.out.println("Not a prime");
-			startValue = list.get(list.size() - 1);
-//			System.out.println(startValue);
-			nextValue = list.get(list.size() - 2) + startValue;
-//			System.out.println(nextValue);
+			startValue = list.get(list.size() - 1) + list.get(list.size() - 2);
+			nextValue = list.get(list.size() - 1) + startValue;
 		}
 
-		ArrayList<Integer> finalList = new ArrayList<Integer>();
-//		int count = 0;
-//		finalList.add(startValue);
-//		while (count < limit) {
-//			finalList.add(nextValue);
-//			nextValue = startValue + nextValue;
-//			startValue = nextValue - startValue;
-//		}
-//		System.out.println(finalList);
-		return finalList;
+		int count = 1;
+		System.out.println(startValue);
+		while (count < limit) {
+			System.out.println(nextValue);
+			nextValue = startValue + nextValue;
+			startValue = nextValue - startValue;
+			count++;
+		}
+
 	}
 
 	public static void main(String[] args) {
 		userinput();
 		find();
-		System.out.println(sequence(find()));
+		sequence(find());
 	}
 }
