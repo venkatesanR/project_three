d06a3ac46c005e5a35bd463e51de634eccbe0a06
mvenkatesan <mohanakrishnan0592@gmail.com>
2016-12-13 17:30:32 +0530

    spiral printing

--- training/src/main/java/com/techland/training/krishna/Matrix.java
+++ training/src/main/java/com/techland/training/krishna/Matrix.java
@@ -2,8 +2,8 @@ package com.techland.training.krishna;
 
 public class Matrix {
 		
-	static int r=3;
-	static int c=3;
+	static int r=5;
+	static int c=5;
 	static int[][] matrix = null;
 	
 	public static int[][] arrayDecleration() {
@@ -12,12 +12,28 @@ public class Matrix {
 			matrix[0][0]=1;
 			matrix[0][1]=2;
 			matrix[0][2]=3;
-			matrix[1][0]=4;
-			matrix[1][1]=5;
-			matrix[1][2]=6;
-			matrix[2][0]=7;
-			matrix[2][1]=8;
-			matrix[2][2]=9;	
+			matrix[0][3]=4;
+			matrix[0][4]=5;
+			matrix[1][0]=6;
+			matrix[1][1]=7;
+			matrix[1][2]=8;
+			matrix[1][3]=9;
+			matrix[1][4]=10;
+			matrix[2][0]=11;
+			matrix[2][1]=12;
+			matrix[2][2]=13;
+			matrix[2][3]=14;
+			matrix[2][4]=15;
+			matrix[3][0]=16;
+			matrix[3][1]=17;
+			matrix[3][2]=18;
+			matrix[3][3]=19;
+			matrix[3][4]=20;
+			matrix[4][0]=21;
+			matrix[4][1]=22;
+			matrix[4][2]=23;
+			matrix[4][3]=24;
+			matrix[4][4]=25;
 		}
 		return matrix;
 
--- training/src/main/java/com/techland/training/krishna/SpiralPrinting.java
+++ training/src/main/java/com/techland/training/krishna/SpiralPrinting.java
@@ -2,66 +2,76 @@ package com.techland.training.krishna;
 
 import java.util.ArrayList;
 
-import javax.swing.text.Position;
-
 public class SpiralPrinting {
-	
-	static SpiralPrinting position = new SpiralPrinting();
-	private int rowStart;
-	private int columnStart;
-	
-	public static SpiralPrinting increment(int rowStart, int columnStart){	
-		while (columnStart != Matrix.c){
-			System.out.println(Matrix.matrix[rowStart][columnStart]);
-			columnStart = columnStart+1;
-		}
-		rowStart=rowStart+1;
-		columnStart=columnStart-1;
-		
-		while (rowStart != Matrix.r){
-			System.out.println(Matrix.matrix[rowStart][columnStart]);
-			rowStart=rowStart+1;
-		}
-		rowStart=rowStart-1;
-		
-		position.rowStart=rowStart;
-		position.columnStart=columnStart;
-		return position;
-	}
-	
-	public static void decrement(int rowStart,int columnStart){
-		rowStart = position.rowStart;
-		columnStart = position.columnStart;
-		
-		columnStart=columnStart-1;
-		while (columnStart <= Matrix.c){
-			if (columnStart < 0){
-				break;
-			}
-			System.out.println(Matrix.matrix[rowStart][columnStart]);
-			columnStart = columnStart-1;
+	static ArrayList<Integer> rowList = new ArrayList<Integer>();
+	static ArrayList<Integer> columnList = new ArrayList<Integer>();
+	static int rowStart = 0;
+	static int columnStart = 0;
+	static int rowEnd = Matrix.r - 1;
+	static int columnEnd = Matrix.c - 1;
 
-		}
-		rowStart=rowStart-1;
-		columnStart=columnStart+1;
-		while (rowStart != Matrix.r){
-			if (rowStart < 0){
+	public static void increment(int rowStart, int columnStart) {
+		while (!columnList.contains(columnStart)) {
+			System.out.println(Matrix.matrix[rowStart][columnStart]);
+			columnStart = columnStart + 1;
+			if (columnStart == Matrix.c) {
 				break;
 			}
+		}rowList.add(rowStart);
+		columnStart = columnStart - 1;
+		rowStart = rowStart + 1;
+		
+		while (!rowList.contains(rowStart)) {
 			System.out.println(Matrix.matrix[rowStart][columnStart]);
-			rowStart=rowStart-1;
-		}
+			rowStart = rowStart + 1;
+			if (rowStart == Matrix.r) {
+				break;
+			}
+		}columnList.add(columnStart);
 	}
-	
+
+	public static void decrement(int rowend, int columnend) {
+		columnend = columnend - 1;
+		while (!columnList.contains(columnend)) {
+			System.out.println(Matrix.matrix[rowend][columnend]);
+			columnend = columnend - 1;
+			if (columnend == columnStart - 1) {
+				break;
+			}
+		}rowList.add(rowend);
+		columnend = columnend + 1;
+		rowend = rowend - 1;
+		
+		while (!rowList.contains(rowend)) {
+			System.out.println(Matrix.matrix[rowend][columnend]);
+			rowend = rowend - 1;
+			if (rowend == rowStart - 1) {
+				break;
+			}
+		}columnList.add(columnStart);
+	}
+
 	public static void main(String[] args) {
-		int rowStart=0;
-		int columnStart=0;
 		Matrix.arrayDecleration();
-		increment(rowStart,columnStart);
-		decrement(rowStart,columnStart);
-
-			
-
+		if (Matrix.r == 1) {
+			while (columnStart != Matrix.c) {
+				System.out.println(Matrix.matrix[rowStart][columnStart]);
+				columnStart = columnStart + 1;
+			}
+		} else if (Matrix.c == 1) {
+			while (rowStart != Matrix.r) {
+				System.out.println(Matrix.matrix[rowStart][columnStart]);
+				rowStart = rowStart + 1;
+			}
+		} else {
+			while (rowList.size() < Matrix.r && columnList.size() < Matrix.c) {
+				increment(rowStart, columnStart);
+				decrement(rowEnd, columnEnd);
+				rowStart = rowStart + 1;
+				rowEnd = rowEnd - 1;
+				columnStart = columnStart + 1;
+				columnEnd = columnEnd - 1;
+			}
+		}
 	}
-
 }
