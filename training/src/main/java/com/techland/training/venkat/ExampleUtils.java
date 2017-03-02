a93fb4cbc43dadf92404985433a810146c4a6e9f
Venkatesan Rengasamy <venkatraman1729@gmail.com>
2017-03-02 22:37:17 +0530

    refactor

--- training/src/main/java/com/techland/training/venkat/ExampleUtils.java
+++ training/src/main/java/com/techland/training/venkat/ExampleUtils.java
@@ -1,33 +1,34 @@
 package com.techland.training.venkat;
 
 public class ExampleUtils {
-    public static void main(String args[]) {
-    	String input="yahoo";
-    	char[] data=input.toCharArray();
-    	reArrange(data);
-    }
+	public static void main(String args[]) {
+		String input = "yahoo";
+		char[] data = input.toCharArray();
+		reArrange(data);
+	}
 
-    private static void reArrange(char[] data) {
-    	String response="";
-    	for(int index =0;index <data.length;index++) {
-    		int frequency=frequency(data, data[index],index);
-    		if((frequency > 0 && frequency%2!=0) ) {
-    			response=response.concat(String.valueOf(data[index]));
-    		}else if((frequency > 0 && frequency%2==0) ) {
-        		index=index+frequency-1;
-    		}
-    	}
-    	System.out.println(response);
-    }
-    private static int frequency(char[] data,char input,int pos) {
-    	int count=0;
-    	for(int index =pos;index <data.length;index++) {
-    		if((input ^ data[index])==0){
-    			count++;
-    		}else {
-    			break;
-    		}
-    	}
+	private static void reArrange(char[] data) {
+		String response = "";
+		for (int index = 0; index < data.length; index++) {
+			int frequency = frequency(data, data[index], index);
+			if ((frequency > 0 && frequency % 2 != 0)) {
+				response = response.concat(String.valueOf(data[index]));
+			} else if ((frequency > 0 && frequency % 2 == 0)) {
+				index = index + frequency - 1;
+			}
+		}
+		System.out.println(response);
+	}
+
+	private static int frequency(char[] data, char input, int pos) {
+		int count = 0;
+		for (int index = pos; index < data.length; index++) {
+			if ((input ^ data[index]) == 0) {
+				count++;
+			} else {
+				break;
+			}
+		}
 		return count;
-    }
+	}
 }
