661be33b61ff2b0c511901ec4b726cf4ca2a73a0
vrengasamy <vrengasamy@yume.com>
2017-10-09 12:39:55 +0000

    added circularly linked list

---
+++ common/common-parent/common-utils/src/main/java/com/datastructures/linkedlist/CircularlyLinkedList.java
@@ -0,0 +1,41 @@
+package com.datastructures.linkedlist;
+
+public class CircularlyLinkedList<T> extends LinkedListADT<T> {
+
+	@Override
+	public void add(Object data, int position) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(Object data) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(int position) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
