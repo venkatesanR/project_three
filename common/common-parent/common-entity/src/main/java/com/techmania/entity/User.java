c932d5a966b8daf9095b9bfa50812863d4578ee7
venkatiris <venkatraman1729@gmail.com>
2019-03-02 18:21:29 +0530

    updated hadoop ecosystem module

--- common/common-parent/common-entity/src/main/java/com/techmania/entity/User.java
+++ common/common-parent/common-entity/src/main/java/com/techmania/entity/User.java
@@ -2,33 +2,37 @@ package com.techmania.entity;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
-	private Integer userId;
-	private String userName;
-	private String password;
+/***
+ * Simple command
+ *
+ */
+public class User implements Serializable {
+    private Integer userId;
+    private String userName;
+    private String password;
 
-	public Integer getUserId() {
-		return userId;
-	}
+    public Integer getUserId() {
+        return userId;
+    }
 
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
