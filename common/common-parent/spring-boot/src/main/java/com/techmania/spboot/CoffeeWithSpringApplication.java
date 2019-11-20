694f9f12959a11751052fa4490321af6bb1c220b
Venkatesan Rengasamy <venkatraman1729@gmail.com>
2019-11-20 22:28:55 +0530

    TECHMAN01:CI Build test commit

--- common/common-parent/spring-boot/src/main/java/com/techmania/spboot/CoffeeWithSpringApplication.java
+++ common/common-parent/spring-boot/src/main/java/com/techmania/spboot/CoffeeWithSpringApplication.java
@@ -3,6 +3,9 @@ package com.techmania.spboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Ignore this is for CI baby
+ */
 @SpringBootApplication
 public class CoffeeWithSpringApplication {
     public static void main(String[] args) {
